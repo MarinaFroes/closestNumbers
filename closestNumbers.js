6ce3d2edf6358961468f9e7384cbd6ee6217d9f4
MFAC <ninafroes@gmail.com>
2019-07-22 14:12:59 +0200

    feat: Remove inner for loop to improve performance

--- closestNumbers.js
+++ closestNumbers.js
@@ -1,30 +1,26 @@
-const closestNumbers = (arr) => {
+const closestNumbers = arr => {
   let difference, smallestDif;
   const difAndPairs = [];
   const pairs = [];
 
-  const sortedArr = arr.sort((a,b) => a-b);
+  const sortedArr = arr.sort((a, b) => a - b);
 
-  for(let i = 0; i < arr.length; i++){
+  for (let i = 0; i < arr.length; i++) {
+    difference = Math.abs(sortedArr[i + 1] - sortedArr[i]);
 
-    for(let j = i + 1; j < arr.length; j++){
-      // Find absolute value of the difference between pairs
-      difference = Math.abs(sortedArr[j] - sortedArr[i]);
+    // Store the smallest difference between pairs
+    if (smallestDif === undefined || smallestDif > difference) {
+      smallestDif = difference;
+    }
 
-      // Store the smallest difference between pairs
-      if(smallestDif === undefined || smallestDif > difference) {
-        smallestDif = difference;
-      }
-
-      // Push difference and pairs to the difAndPairs array
-      difAndPairs.push([difference, sortedArr[i], sortedArr[j]]);
-    };
-
-  };
+    // Push difference and pairs to the difAndPairs array
+    difAndPairs.push([difference, sortedArr[i], sortedArr[i + 1]]);
+  }
 
   // Loop over the difAndPairs and push to pairs only the ones that have the smallest difference
-  for(let k = 0; k < difAndPairs.length; k++){
-    difAndPairs[k][0] === smallestDif && pairs.push(difAndPairs[k][1], difAndPairs[k][2]);
+  for (let k = 0; k < difAndPairs.length; k++) {
+    difAndPairs[k][0] === smallestDif &&
+      pairs.push(difAndPairs[k][1], difAndPairs[k][2]);
   }
 
   // Output all pairs in ascending order
@@ -33,6 +29,32 @@ const closestNumbers = (arr) => {
 
 // Tests
 console.log(closestNumbers([5, 2, 3, 4, 1]));
-console.log(closestNumbers([-20, -3916237, -3620601, 7374819, -7330761, 30, 6246457, -6461594, 266854]));
-console.log(closestNumbers([-20, -3916237, -3620601, 7374819, -7330761, 30, 6246457, -6461594, 266854, -520, -470]));
+console.log(
+  closestNumbers([
+    -20,
+    -3916237,
+    -3620601,
+    7374819,
+    -7330761,
+    30,
+    6246457,
+    -6461594,
+    266854
+  ])
+);
+console.log(
+  closestNumbers([
+    -20,
+    -3916237,
+    -3620601,
+    7374819,
+    -7330761,
+    30,
+    6246457,
+    -6461594,
+    266854,
+    -520,
+    -470
+  ])
+);
 console.log(closestNumbers([5, 4, 3, 2]));
